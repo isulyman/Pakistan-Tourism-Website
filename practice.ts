62a7e1b4d8195b395ea72c484d22240c48b2ce44
isulyman <islymanali@gmail.com>
2023-10-11 20:11:26 +0500

    first commit

---
+++ practice.ts
@@ -0,0 +1,20 @@
+// // // // let a = "admin";
+// // // // let b = "administration";
+// // // for (let a = 1; a < 10; a++) {
+// // //   console.log(a);
+// // // }
+// // let user = "Akmal";
+// // let message = "Hello";
+// // console.log(`Aslam o Alaikum ${message} ${user}`);
+
+// function fullname(firstname: string, lastname: string) {
+//   return `${firstname} ${lastname}`;
+// }
+
+// let myname = `My Name is ${fullname("Salman", "Ali")}`;
+// console.log(myname);
+// console.log(`${fullname("Faiq", "Ali")}`);
+let add = (a: number, b: number, c: number) => {
+  return a + b + c;
+};
+console.log(add(2, 4, 6));
